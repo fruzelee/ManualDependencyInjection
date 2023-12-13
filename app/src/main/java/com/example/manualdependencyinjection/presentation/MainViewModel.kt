dab74e8d8c83c1f0541e358e8196958f74f79853
fazle <fazlerabbicse@gmail.com>
2023-12-13 12:07:07 +0600

    feat(presentation): add MainViewModel

---
+++ app/src/main/java/com/example/manualdependencyinjection/presentation/MainViewModel.kt
@@ -0,0 +1,19 @@
+package com.example.manualdependencyinjection.presentation
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.manualdependencyinjection.domain.AuthRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val authRepository: AuthRepository
+) : ViewModel() {
+    fun login() {
+        viewModelScope.launch {
+            authRepository.login("test@test.com", "test123")
+        }
+    }
+}
