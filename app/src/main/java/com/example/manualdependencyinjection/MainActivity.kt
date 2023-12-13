589c99f67457f329645787b020fb82782c4d3bc5
fazle <fazlerabbicse@gmail.com>
2023-12-13 12:09:25 +0600

    feat: update the MainActivity

--- app/src/main/java/com/example/manualdependencyinjection/MainActivity.kt
+++ app/src/main/java/com/example/manualdependencyinjection/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.manualdependencyinjection.presentation.MainViewModel
 import com.example.manualdependencyinjection.ui.theme.ManualDependencyInjectionTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +20,13 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ManualDependencyInjectionTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Everyone")
+                val viewModel = hiltViewModel<MainViewModel>()
+                Button(onClick = {
+                    viewModel.login()
+                }) {
+                    Text(text = "Login")
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Salam $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ManualDependencyInjectionTheme {
-        Greeting("Android")
     }
 }
